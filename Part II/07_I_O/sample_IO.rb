54b977a70584975739a5c9750ceba8496b3ae423
jhonatancasale <jhonatan@jhonatancasale.com>
2017-04-08 19:44:14 -0300

    Add contente about Input and Output

---
+++ Part II/07_I_O/sample_IO.rb
@@ -0,0 +1,15 @@
+print "Enter a value: "
+first_num = gets.to_i
+
+print "Another value: "
+second_value = gets.to_i
+
+puts first_num.to_s + " + " + second_value.to_s + " = " + 
+  (first_num + second_value).to_s
+
+
+name = "Name"
+float = 3.141518
+integer = 123
+
+printf "%s, %.3f, %d\n", name, float, integer
