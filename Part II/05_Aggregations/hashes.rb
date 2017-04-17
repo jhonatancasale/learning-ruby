c5aaac4120801e82e936ad33687f710b77bf9a40
jhonatancasale <jhonatan@jhonatancasale.com>
2017-04-17 17:51:19 -0300

    Add more content

--- Part II/05_Aggregations/hashes.rb
+++ Part II/05_Aggregations/hashes.rb
@@ -1,3 +1,5 @@
+# order are preserved
+
 numbers = {1 => 'one', 2 => 'two'}
 puts numbers
 puts numbers[1]
@@ -10,3 +12,24 @@ puts stuff[:string]
 another_stuff = {array: [1, 2, 3], string: 'Hi, mon!'} # 1.9
 puts another_stuff
 puts another_stuff[:string]
+
+hash = Hash.new
+p hash.length
+hash[:first] = 1
+hash[:seconf] = 2
+
+hash.each_pair do |key, value|
+  puts "#{key} => #{value}"
+end
+
+p hash[:third] # => nil
+another_hash = Hash.new(:void) # :void as default value when key are not in hash
+p another_hash[:third] # => :void
+
+h = {one: "one"}
+p h
+#p {one: "one"}
+#hashes.rb:32: syntax error, unexpected ':', expecting '}'
+p ({one: "one"})
+p :one => "one"
+p one: "one"  # preferred
